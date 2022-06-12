2fb03f17b4ce7209da94b17df09f4323c3e3ff84
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-06-12 16:41:06 -0300

    Fix bug

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 import HttpStatus from "http-status-codes";
 
 export default async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : "";
+
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(HttpStatus.UNAUTHORIZED).send();

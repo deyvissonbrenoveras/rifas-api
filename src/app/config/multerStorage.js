cc8aa33c1086cbb40fd42b7ea3e4cbe713807340
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-06-13 00:03:46 -0300

    multer

---
+++ src/app/config/multerStorage.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+import crypto from "crypto";
+
+export default multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "tmpUploads/");
+  },
+  filename: function (req, file, cb) {
+    const fileExt = file.originalname.split(".")[1];
+
+    const newFileName = crypto.randomBytes(64).toString("hex");
+
+    cb(null, `${newFileName}.${fileExt}`);
+  },
+});
--- src/app/routes/authenticatedRoutes.js
+++ src/app/routes/authenticatedRoutes.js
@@ -1,10 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 import auth from "../middlewares/auth";
 import RaffleController from "../controllers/RaffleController";
+
+import multerStorage from "../config/multerStorage";
+const maxImages = 3;
+const upload = multer({
+  storage: multerStorage,
+});
 const authenticatedRoutes = Router();
 
 authenticatedRoutes.use(auth);
 
 authenticatedRoutes.post("/raffles", RaffleController.store);
+authenticatedRoutes.post(
+  "/images",
+  upload.array("images", maxImages),
+  (req, res) => {
+    console.log(req.files);
+    res.send();
+  }
+);
 
 export default authenticatedRoutes;
